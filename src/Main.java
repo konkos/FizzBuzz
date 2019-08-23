7bacfe894a96905c0ed501a109214ff65ce3189d
kongames <kon_kostas@windowslive.com>
2019-08-23 18:43:19 +0300

    Finished Project

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,26 @@ public class Main {
 		
 		Scanner in = new Scanner(System.in);
 		
+		String out = "";
+		
+		//Defines the end of the loop
+		int limit;
+		do {
+			System.out.println("Give the Limit");
+			limit = in.nextInt();
+		}while(limit<=0);
+		
+		for(int i=1;i<limit;i++) {
+			if(i%3 == 0)
+				out+="Fizz";
+			if(i%5 == 0)
+				out+="Buzz";
+			if(i%3 != 0 && i%5 != 0)
+				out+= ""+i;
+			
+			System.out.println(out);
+			out="";
+		}
 
 	}
 
